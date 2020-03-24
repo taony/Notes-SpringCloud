d22251c0bbd27d374d972c3dc73ef638061955d1
taony <taonyzhang@gmail.com>
2020-03-24 22:22:48 +0800

    add springcloud eureka cluster demo

---
+++ src/springcloud-eureka-cluster/springcloud-eureka-server01/src/main/java/org/devnotes/eureka/EurekaServer01.java
@@ -0,0 +1,11 @@
+package org.devnotes.eureka;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer01 {
+
+}
---
+++ src/springcloud-eureka-cluster/springcloud-eureka-server02/src/main/java/org/devnotes/eureka/EurekaServer02.java
@@ -0,0 +1,10 @@
+package org.devnotes.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer02 {
+
+}
